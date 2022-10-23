f2bfc23044d0f7f166c9fd79e25a83f8a0c67080
afshinpy <afshinpht1379@gmail.com>
2022-10-23 13:16:48 +0330

    add AbstractRepository class

---
+++ src/main/java/com/project/doctorhub/base/repository/AbstractRepository.java
@@ -0,0 +1,21 @@
+package com.project.doctorhub.base.repository;
+
+
+import com.project.doctorhub.base.model.BaseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+import java.util.List;
+
+@NoRepositoryBean
+public interface AbstractRepository<ENTITY extends BaseEntity<PK>, PK extends Serializable>
+        extends JpaRepository<ENTITY, PK> {
+
+    Page<ENTITY> findAllByIsDeleted(Boolean isDeleted, Pageable pageable);
+
+    List<ENTITY> findAllByIsDeleted(Boolean isDeleted);
+
+}
