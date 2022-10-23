c95b37dfd939fcee9a3a6f424e7fd1995288e5bd
afshinpy <afshinpht1379@gmail.com>
2022-10-23 13:16:28 +0330

    add BaseEntity class

---
+++ src/main/java/com/project/doctorhub/base/model/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.project.doctorhub.base.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity<PK extends Serializable> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private PK id;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    private Boolean isDeleted;
+
+}
