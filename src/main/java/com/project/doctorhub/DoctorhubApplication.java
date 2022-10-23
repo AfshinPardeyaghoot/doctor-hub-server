50e30e5cb35279ee6df8ea2d98cc69551ff385f1
afshinpy <afshinpht1379@gmail.com>
2022-10-23 12:03:34 +0330

    initial commit

---
+++ src/main/java/com/project/doctorhub/DoctorhubApplication.java
@@ -0,0 +1,13 @@
+package com.project.doctorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorhubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/doctorhub/DoctorhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.doctorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
