5d5280f390e3b4136cbe41ce995b7b6c41fe6f01
afshinpy <afshinpht1379@gmail.com>
2022-10-26 10:18:05 +0330

    add implementing userDetailsService to userService

--- src/main/java/com/project/doctorhub/auth/service/UserService.java
+++ src/main/java/com/project/doctorhub/auth/service/UserService.java
@@ -1,13 +1,19 @@
 package com.project.doctorhub.auth.service;
 
+import com.project.doctorhub.auth.model.ApplicationUserDetails;
 import com.project.doctorhub.auth.model.User;
 import com.project.doctorhub.auth.repository.UserRepository;
 import com.project.doctorhub.base.exception.NotFoundException;
 import com.project.doctorhub.base.service.AbstractCrudService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService extends AbstractCrudService<User, Long, UserRepository> {
+public class UserService
+        extends AbstractCrudService<User, Long, UserRepository>
+        implements UserDetailsService {
 
     private final UserRepository userRepository;
 
@@ -18,6 +24,14 @@ public class UserService extends AbstractCrudService<User, Long, UserRepository>
 
     public User findByPhone(String phone) {
         return userRepository.findByPhone(phone)
-                .orElseThrow(()-> new NotFoundException("کاربر یافت نشد!"));
+                .orElseThrow(() -> new NotFoundException("کاربر یافت نشد!"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String phone) throws UsernameNotFoundException {
+        User user = userRepository.findByPhone(phone)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found!"));
+
+        return new ApplicationUserDetails(user);
     }
 }
