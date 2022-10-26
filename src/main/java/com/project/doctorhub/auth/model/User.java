6d89103574e57a97a07641e774350c9fc5cc11f5
afshinpy <afshinpht1379@gmail.com>
2022-10-26 10:17:34 +0330

    add method to get user roles as granted authority

--- src/main/java/com/project/doctorhub/auth/model/User.java
+++ src/main/java/com/project/doctorhub/auth/model/User.java
@@ -5,10 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
@@ -20,8 +23,14 @@ public class User extends BaseEntity<Long> {
 
     @Column(name = "phone", unique = true, nullable = false)
     private String phone;
-
     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private Set<UserRole> userRoles = new HashSet<>();
+
+    public Set<GrantedAuthority> getRoles() {
+        return userRoles.stream()
+                .map(UserRole::getRole)
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+    }
 
 }
